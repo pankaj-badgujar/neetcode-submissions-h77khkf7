fc65e3d767af2f612a81a7789cdc627d992f2b0b
Pankaj Badgujar <badgujarpankaj24@gmail.com>
2026-04-10 14:30:35 -0700

    Add: permutation-string - submission-0

---
+++ Data Structures & Algorithms/permutation-string/submission-0.java
@@ -0,0 +1,40 @@
+class Solution {
+    private boolean matchFreqMaps(Map<Character, Integer> m1, Map<Character, Integer> m2){
+        for(Map.Entry e : m1.entrySet()){
+            char m1Key = (char)e.getKey();
+            if(m2.get(m1Key) != e.getValue()){
+                return false;
+            }
+        }
+        return true;
+    }
+    public boolean checkInclusion(String s1, String s2) {
+        Map<Character, Integer> m2 = new HashMap<>();
+        Map<Character, Integer> m1 = new HashMap<>();
+
+        for(char c : s1.toCharArray()){
+            m1.merge(c, 1, Integer::sum);
+        }
+
+
+        for(int l = 0, r = 0; r < s2.length(); r++){
+            if (r > l + s1.length() - 1){
+                char lc = s2.charAt(l);
+                m2.merge(lc, -1, Integer::sum);
+                if(m2.get(lc) == 0){
+                    m2.remove(lc);
+                }
+                l++;
+            }
+            char rc = s2.charAt(r);
+            m2.merge(rc, 1, Integer::sum);
+
+            if(r - l + 1 == s1.length()){
+                if (matchFreqMaps(m1, m2)){
+                    return true;
+                }
+            }
+        }
+        return false;        
+    }
+}
