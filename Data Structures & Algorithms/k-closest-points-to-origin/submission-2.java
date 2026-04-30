912433e7994cd552cabb746551e20d165ecc849c
Pankaj Badgujar <badgujarpankaj24@gmail.com>
2026-04-29 17:27:26 -0700

    Add: k-closest-points-to-origin - submission-2

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-2.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[][] kClosest(int[][] points, int k) {
+
+        PriorityQueue<int[]> maxHeap = new PriorityQueue<>(
+            (a, b) ->
+                    (b[0] * b[0] + b[1] * b[1]) -
+                    (a[0] * a[0] + a[1] * a[1])
+            );
+
+        for (int[] point : points){
+            maxHeap.offer(point);
+
+            if (maxHeap.size() > k){
+                maxHeap.poll();
+            }
+        }
+
+        // now what we are left with is points with least distance because we polled the most distance
+        int[][] result = new int[maxHeap.size()][2];
+
+        int i = 0;
+        while (!maxHeap.isEmpty()){
+            result[i++] = maxHeap.poll();
+        }
+        return result;
+    }
+}
