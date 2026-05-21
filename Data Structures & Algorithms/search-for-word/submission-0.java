69d89a8ec4b33cf173233e3e7ae0a2bcb6a7490e
Pankaj Badgujar <badgujarpankaj24@gmail.com>
2026-05-20 17:03:04 -0700

    Add: search-for-word - submission-0

---
+++ Data Structures & Algorithms/search-for-word/submission-0.java
@@ -0,0 +1,43 @@
+class Solution {
+    private Set visited = new HashSet<Pair<Integer, Integer>>();
+
+    private boolean dfs(char[][] board, int i, int j, String word, int idx){
+        if (idx == word.length()){
+            return true;
+        }
+
+        // if i have reached an out of bound cell, then just return false
+        Pair pair = new Pair(i, j);
+        if (
+            i < 0 || i >= board.length ||
+            j < 0 || j >= board[i].length ||
+            word.charAt(idx) != board[i][j] ||
+            visited.contains(pair)
+        ){
+            return false;
+        }
+
+        visited.add(pair);
+
+        boolean res = dfs(board, i - 1, j, word, idx + 1) ||
+        dfs(board, i + 1, j, word, idx + 1) ||
+        dfs(board, i, j - 1, word, idx + 1) ||
+        dfs(board, i, j + 1, word, idx + 1);
+
+        visited.remove(pair);
+        return res;
+    }
+
+    public boolean exist(char[][] board, String word) {
+        for (int i = 0 ; i < board.length; i++){
+            for (int j = 0; j < board[i].length; j++){
+
+                if(dfs(board, i, j, word, 0)){
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+}
