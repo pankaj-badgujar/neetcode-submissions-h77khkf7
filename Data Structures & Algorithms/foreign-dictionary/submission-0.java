2443e48e2c65faf300087dd1a8473c72f0ba79af
Pankaj Badgujar <badgujarpankaj24@gmail.com>
2026-06-14 07:59:07 -0700

    Add: foreign-dictionary - submission-0

---
+++ Data Structures & Algorithms/foreign-dictionary/submission-0.java
@@ -0,0 +1,70 @@
+class Solution {
+    Map<Character, Set<Character>> adj;
+    Map<Character, Boolean> visited;
+    List<Character> res;
+
+    public String foreignDictionary(String[] words) {
+        adj = new HashMap<>();
+        for (String word : words) {
+            for (char c : word.toCharArray()) {
+                adj.putIfAbsent(c, new HashSet<>());
+            }
+        }
+
+        for (int i = 0; i < words.length - 1; i++){
+            String w1 = words[i], w2 = words[i + 1];
+
+            int minLength =  Math.min(w1.length(), w2.length());
+
+            if (
+            w1.length() > w2.length() &&
+            w1.substring(0, minLength).equals(w2.substring(0, minLength))
+            ){
+                return "";
+            }
+
+            for (int j = 0; j < minLength; j++){
+                char c1 = w1.charAt(j);
+                char c2 = w2.charAt(j);
+
+                if (c1 != c2){
+                    adj.get(c1).add(c2);
+                    break;
+                }
+            }
+        }
+        
+        visited = new HashMap<>();
+        res = new ArrayList<>();
+
+        for (char c : adj.keySet()){
+            if (dfs(c)){
+                return "";
+            }
+        }
+
+        String result = "";
+        for (char c : res){
+            result = c + result;
+        }
+        return result;
+    }
+
+    private boolean dfs(char c){
+        if (visited.containsKey(c)){
+            return visited.get(c);
+        }
+
+        visited.put(c, true);
+
+        for (char nei : adj.get(c)){
+            if (dfs(nei)){
+                return true;
+            }
+        }
+
+        visited.put(c, false);
+        res.add(c);
+        return false;
+    }
+}
