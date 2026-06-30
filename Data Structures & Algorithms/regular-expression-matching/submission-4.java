095e2dfb93e3003299bbf53b2d0ffe3cc04c3181
Pankaj Badgujar <badgujarpankaj24@gmail.com>
2026-06-29 19:09:23 -0700

    Add: regular-expression-matching - submission-4

---
+++ Data Structures & Algorithms/regular-expression-matching/submission-4.java
@@ -0,0 +1,38 @@
+class Solution {
+    String s, p;
+    Boolean[][] dp;
+    public boolean isMatch(String s, String p) {
+        this.s = s;
+        this.p = p;
+
+        dp = new Boolean[s.length() + 1][p.length() + 1];
+        return dfs(0,0);
+    }
+
+    private boolean dfs(int i, int j){
+        // base case
+        // if i and j are both out of bounds together, we reached the end
+        if (i >= s.length() && j >= p.length()) return true;
+        if (j >= p.length()) return false;
+
+        if (i < s.length() && dp[i][j] != null){
+            return dp[i][j];
+        }
+        
+        boolean match = i < s.length() && (s.charAt(i) == p.charAt(j) || p.charAt(j) == '.');
+
+        // check * first
+        if (j + 1 < p.length() && p.charAt(j + 1) == '*'){
+            // two choices
+
+            dp[i][j] = dfs(i, j + 2) || (match && dfs(i + 1, j));
+            return dp[i][j];
+        }
+        if (match){
+            dp[i][j] = dfs(i + 1, j + 1);
+            return dp[i][j];
+        }
+        dp[i][j] = false;
+        return false;
+    }
+}
