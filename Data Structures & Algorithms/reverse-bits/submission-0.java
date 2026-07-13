67a5e37d87af28ce39245690318cdc348ec8f47a
Pankaj Badgujar <badgujarpankaj24@gmail.com>
2026-07-13 10:39:57 -0700

    Add: reverse-bits - submission-0

---
+++ Data Structures & Algorithms/reverse-bits/submission-0.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int reverseBits(int n) {
+        int res = 0;
+        
+        for (int i = 0; i < 32; i++){
+            // get ith bit
+            int bit = (n >> i) & 1;
+            res |= (bit <<  (31 - i));
+        }
+        return res;
+        
+    }
+}
