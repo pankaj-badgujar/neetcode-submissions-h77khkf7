13fd1660f5350b852ee77e69d8c3302c082283c1
Pankaj Badgujar <badgujarpankaj24@gmail.com>
2026-07-13 09:19:21 -0700

    Add: count-squares - submission-1

---
+++ Data Structures & Algorithms/count-squares/submission-1.java
@@ -0,0 +1,37 @@
+class CountSquares {
+    record Cords(int x, int y){};
+    List<Cords> pts;
+    Map<Cords, Integer> map;
+
+    public CountSquares() {
+        map = new HashMap<>();
+        pts = new ArrayList<>();
+    }
+    
+    public void add(int[] point) {
+        Cords c = new Cords(point[0], point[1]);
+        map.merge(c, 1, Integer::sum);
+        pts.add(c);
+    }
+    
+    public int count(int[] point) {
+        int count = 0;
+        int px = point[0], py = point[1];
+        
+        for (Cords c : pts){
+            if (
+                Math.abs(px - c.x) != Math.abs(py - c.y) ||
+                px == c.x || py == c.y
+            ){
+                continue;
+            }
+            // diagonal found
+            // then we find matches and add counts
+            Cords c1 = new Cords(px, c.y);
+            Cords c2 = new Cords(c.x, py);
+            count += map.getOrDefault(c1, 0) * map.getOrDefault(c2, 0);
+            
+        }
+        return count;
+    }
+}
